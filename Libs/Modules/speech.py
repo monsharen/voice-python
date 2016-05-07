38e00ec5b9da92fad2779ae36301b124b0c0ee49
Axel Ericsson <axel.ericsson@gmail.com>
2016-05-07 15:41:30 +0100

    Refactored code, Changed titration scheme,

--- Libs/Modules/speech.py
+++ Libs/Modules/speech.py
@@ -1,4 +1,5 @@
 __author__ = 'a.ericsson'
+__author__ = 'a.ericsson'
 
 import sys, os
 from pocketsphinx.pocketsphinx import *
@@ -6,20 +7,21 @@ from sphinxbase.sphinxbase import *
 
 Root = "C:\\Users\\a.ericsson\\PycharmProjects\\SpeechAnalytics\\Voice_Python\\Libs"
 os.chdir(Root)
+sys.path.append(Root + '\\Modules')
 modeldir = "..\\Model"
 datadir = "..\\Datasets\\TestSet"
+import word_align
 
-def speechanalytics(type,OOG=1e+1):
-    stream = stream = open(os.path.join(datadir, "Test.12.wav"), "rb")
-    config = detectionconfig(type,OOG)
 
+def speechanalytics(audiofile="Test.12.wav",type="kws",kwsfile="",OOG=None,**kwargs):
+    stream = stream = open(os.path.join(datadir, audiofile), "rb")
+    config = detectionconfig(type, OOG, kwsfile)
     if type == "kws" or "keyphrase":
-        result = keyword_spotting(config,stream)
-
+        result = keyword_spotting(config, stream, OOG)
     elif type == "lm":
-        result = transcribe(config,stream)
+        result = transcribe(config, stream)
+    return result
 
-    return  result
 
 # Create a decoder with certain model
 def defaultconfig():
@@ -28,13 +30,14 @@ def defaultconfig():
     config.set_string('-dict', os.path.join(modeldir, 'en-us/cmudict-en-us.dict'))
     return config
 
-def detectionconfig(type,OOG):
+
+def detectionconfig(type, OOG,kwsfile):
     config = defaultconfig()
 
     if type == "kws":
-        config.set_string('-kws', os.path.join(Root, 'kws.txt'))
-        config.set_float('-kws_threshold', OOG)
-
+        config.set_string('-kws', os.path.join(Root, kwsfile))
+        if OOG != None:
+            config.set_float('-kws_threshold', OOG)
         return config
 
     elif type == "keyphrase":
@@ -47,7 +50,8 @@ def detectionconfig(type,OOG):
         config.set_string('-lm', os.path.join(modeldir, 'en-us/en-us.lm.bin'))
         return config
 
-def transcribe(config,stream):
+
+def transcribe(config, stream):
     decoder = Decoder(config)
     decoder.start_utt()
     while True:
@@ -55,12 +59,13 @@ def transcribe(config,stream):
         if buf:
             decoder.process_raw(buf, False, False)
         else:
-             break
+            break
     decoder.end_utt()
     hypothesis = decoder.hyp()
     return hypothesis.hypstr
 
-def keyword_spotting(config,stream):
+
+def keyword_spotting(config, stream, OOG,threshold=100):
     result = []
     decoder = Decoder(config)
     decoder.start_utt()
@@ -72,17 +77,9 @@ def keyword_spotting(config,stream):
         else:
             break
         if decoder.hyp() != None:
-            result.append([(seg.word, seg.prob, seg.start_frame, seg.end_frame) for seg in decoder.seg()])
+            for seg in decoder.seg():
+                if seg.prob > threshold:
+                    result.append([seg.word, seg.prob, seg.start_frame, seg.end_frame])
             decoder.end_utt()
             decoder.start_utt()
     return result
-
-if __name__ == "__main__":
-    results = []
-    kwsOog = [1e-50,1e-40,1e-30,1e-20,1e-10,1e+1,1e+10,1e+20,1e+30,1e+30,1e+40,1e+50]
-
-    for i in kwsOog:
-        result = speechanalytics("kws",i)
-        results.append(result)
-    for i in results:
-        print(i)
