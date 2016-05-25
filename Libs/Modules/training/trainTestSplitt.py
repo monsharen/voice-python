5e46fe70a6e3833766ee5d2a7266b9443fc766a7
Axel Ericsson <axel.ericsson@gmail.com>
2016-05-25 20:27:15 +0100

    Updated Pipeline

---
+++ Libs/Modules/training/trainTestSplitt.py
@@ -0,0 +1,20 @@
+__author__ = 'a.ericsson'
+
+## method to create train test split, input train,test portion
+### read the file ide file, create an int split
+### cut original file and pass to test folder, read and merge all subs into one file.
+### adjust fileid file and transcription file
+
+def trainTestSplit(train=0.6,test=0.6,fileIds=None,subArray=None):
+    size =  len(subArray)
+    trainIndex = int(train * size)
+    trainFiles = subArray[0:trainIndex]
+    testFiles  = subArray[trainIndex:size]
+    return [trainFiles,testFiles]
+
+def generateTestTranscript(testFiles,testModelFolder):
+    testTranscription=" ".join( [element[2] for element in testFiles] )
+    TranscriptionFile = open(testModelFolder + "transcription.txt" ,'w',encoding='utf8')
+    TranscriptionFile.write(testTranscription)
+    TranscriptionFile.close()
+
