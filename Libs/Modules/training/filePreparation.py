66864656014e53d5225d974447fa153934f9b6a2
Axel Ericsson <axel.ericsson@gmail.com>
2016-06-01 21:11:54 +0100

    Updated Pipeline

--- Libs/Modules/training/filePreparation.py
+++ Libs/Modules/training/filePreparation.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     fileIdsInputFile = outputFolder + "fileids.txt"
     transcriptionInputFile = outputFolder + "transcription.txt"
     rawTranscription = outputFolder + "rawtranscription.txt"
+    testTranscription = testModelFolder + "transcription.txt"
     kwsfile = outputFolder + "kwsfile.txt"
     refsfile = outputFolder + "refs.txt"
     #  End config
@@ -48,7 +49,7 @@ if __name__ == "__main__":
     train, test = trainTestSplit(fileIds=fileIdsInputFile,subArray=subArray)
 
     print("Generating test files ...")
-    generateRawTranscript(test,testModelFolder=testModelFolder)
+    generateRawTranscript(test,testTranscription)
     start = test[0][0]
     end = test[-1][1]
     generateAudioFiles(testModelFolder,origAudioFile,start,end,trainingSet)
@@ -67,7 +68,7 @@ if __name__ == "__main__":
     print("Test folder: " + testModelFolder)
 
     print("Processing words...")
-    generateRawTranscript(subArray,inputFolder)
+    generateRawTranscript(subArray,rawTranscription)
 
     subProcessStartedTime = time.time()
     words = extraction(rawTranscription, dictionaryFile)
