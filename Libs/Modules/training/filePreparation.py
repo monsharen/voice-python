b697b65a6f0369e3f9b5031427473f7689b17940
Axel Ericsson <axel.ericsson@gmail.com>
2016-06-01 20:31:58 +0100

    Updated Pipeline

--- Libs/Modules/training/filePreparation.py
+++ Libs/Modules/training/filePreparation.py
@@ -1,7 +1,8 @@
-import os
+import os,sys,time
 
 from Modules.training.trainingFilesUtil import *
 from Modules.training.trainTestSplitt import *
+from Modules.keywordExtraction import extraction,randomSampling,reference
 
 if __name__ == "__main__":
 
@@ -13,13 +14,21 @@ if __name__ == "__main__":
     #trainingSet = "arctic"
     inputFolder = os.path.realpath('../../../') + "\\Datasets\TrainingSet\\" + trainingSet + "\\"
     testModelFolder = os.path.realpath('../../../') + "\\Datasets\TestSet\\" + trainingSet + "\\"
+    root = os.path.realpath('..')  # sys.path[0]
+    os.chdir(root)
+    sys.path.append(root + '\\Modules')
+    modelFolder = root + "\\Model\\en-us"
     originalAudioFile = inputFolder + trainingSet +'.wav'
     subsFile = inputFolder + trainingSet + '.srt'
+    dictionaryFile = modelFolder + "\\cmudict-en-us.dict"
 
     #  Output
     outputFolder = inputFolder
     fileIdsInputFile = outputFolder + "fileids.txt"
     transcriptionInputFile = outputFolder + "transcription.txt"
+    rawTranscription = outputFolder + "rawtranscription.txt"
+    kwsfile = outputFolder + "kwsfile.txt"
+    refsfile = outputFolder + "refs.txt"
     #  End config
 
     print("Parsing subs from file '" + subsFile + "'...")
@@ -30,6 +39,7 @@ if __name__ == "__main__":
 
     print("Opening transcription file '" + transcriptionInputFile + "'...")
     transcriptionFile = open(transcriptionInputFile, "w", encoding="utf-8")
+    transcriptionRawFile = open(rawTranscription,"w",encoding="utf-8")
 
     print("Opening original audio file '" + originalAudioFile + "'...")
     origAudioFile = wave.open(originalAudioFile, 'r')
@@ -55,3 +65,26 @@ if __name__ == "__main__":
     print("Done")
     print("Training folder: " + outputFolder)
     print("Test folder: " + testModelFolder)
+
+    print("Processing words...")
+    for i in subArray:
+        transcriptionRawFile.write(i[2] + " ")
+    transcriptionRawFile.close()
+
+    subProcessStartedTime = time.time()
+    words = extraction(transcriptionRawFile, dictionaryFile)
+    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
+    print(words)
+
+    print("Processing keywords...")
+    subProcessStartedTime = time.time()
+    keywords = randomSampling (words, 100, phones=[6], kws=kwsfile)
+    kws = open(kwsfile,'rb')
+    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
+    print(keywords)
+
+    print("Processing reference...")
+    subProcessStartedTime = time.time()
+    refs = reference(keyhash=keywords, inputtext=transcriptionFile, refsfile=refsfile)
+    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
+    print(refs)
