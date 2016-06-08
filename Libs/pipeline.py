d12c14e3124e6b27dcb935a0f37309337d82a809
monsharen <monsharen@gmail.com>
2016-06-08 21:41:08 +0100

    trimmed pipeline to only run pipeline stuff

--- Libs/pipeline.py
+++ Libs/pipeline.py
@@ -23,63 +23,59 @@ if __name__ == "__main__":
     testSetFolder = root + "\\Datasets\\" + dataSetFolder + "\\TestSet\\"
     metaDataFolder = root + "\\Datasets\\" + dataSetFolder + "\\MetaData\\"
 
-    trainingSetFolder = root + "\\Datasets\\" + dataSetFolder + "\\TrainingSet\\"
     modelFolder = root + "\\Model\\en-us"
-    fileIdsFile = trainingSetFolder + "fileids.txt"
-    transcriptionInputFile = trainingSetFolder + "transcription.txt"
-    audioInputFile = trainingSetFolder + 'The_Obama_Deception.wav'
+    transcriptionInputFile = testSetFolder + "transcription.txt"
+    audioInputFile = testSetFolder + 'The_Obama_Deception.wav'
 
     dictionaryFile = modelFolder + "\\cmudict-en-us.dict"
     acousticModel = modelFolder + "\\en-us"
 
     kwsfile = metaDataFolder + "kwsfile.txt"
-    optkws = metaDataFolder + "optkws.txt"
-    refsfile = metaDataFolder + "refs.txt"
+    # optkws = metaDataFolder + "optkws.txt"
+    optkws = kwsfile
+    referenceInputFile = metaDataFolder + "reference_file.txt"
 
     print("Executing pipeline")
     print("dictionaryFile: " + dictionaryFile)
     print("transcriptionInputFile: " + transcriptionInputFile)
     print("kwsfile: " + kwsfile)
     print("optkws: " + optkws)
-    print("refsfile: " + refsfile)
+    print("refsfile: " + referenceInputFile)
 
-    print("Processing words...")
-    subProcessStartedTime = time.time()
-    words = extraction(transcriptionInputFile, dictionaryFile)
-    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
-    print(words)
+    # print("Processing words...")
+    # subProcessStartedTime = time.time()
+    # words = extraction(transcriptionInputFile, dictionaryFile)
+    # print("Process took %s seconds" % (time.time() - subProcessStartedTime))
+    # print(words)
 
-    print("Processing keywords...")
-    subProcessStartedTime = time.time()
-    keywords = randomSampling(words, 5, phones=[6], kws=kwsfile)
-    kws = open(kwsfile,'rb')
-    #keywords = [ word for word in " ".join(kws.readlines()).split() ]
-    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
+    print("Reading keywords...")
+    # subProcessStartedTime = time.time()
+    # keywords = randomSampling(words, 5, phones=[6], kws=kwsfile)
+    kws = open(optkws, 'r')
+    keywords = [ word for word in " ".join(kws.readlines()).split() ]
+    # print("Process took %s seconds" % (time.time() - subProcessStartedTime))
     print(keywords)
 
-    print("Processing reference...")
+    print("Reading reference file...")
     subProcessStartedTime = time.time()
-    refs = reference(keyhash=keywords, inputtext=transcriptionInputFile, refsfile=refsfile)
+    referenceFile = open(referenceInputFile, 'r')  # reference(keyhash=keywords, inputtext=transcriptionInputFile, refsfile=refsfile)
+    referenceArray = [word for word in " ".join(referenceFile.readlines()).split()]
     print("Process took %s seconds" % (time.time() - subProcessStartedTime))
-    print(refs)
+    print(referenceArray)
 
-    print("Processing calibration...")
-    subProcessStartedTime = time.time()
-    config = Config(acousticModel, dictionaryFile, audioInputFile, kwsfile)
-    alignments, hyps = calibration(refkeywords=refsfile, config=config, parameter='oog',optkws=optkws)
-    print("Process took %s seconds" % (time.time() - subProcessStartedTime))
-    print(hyps)
-    print(alignments)
+    config = Config(acousticModel, dictionaryFile, audioInputFile, optkws)
+    config.update({"ogg": 1e+30, "kws": "test"})
 
     print("Processing speech analytics...")
     subProcessStartedTime = time.time()
-    adjustedhyp = speechanalytics(config)
+    hypothesis = speechanalytics(config)
     print("Process took %s seconds" % (time.time() - subProcessStartedTime))
-    print(adjustedhyp)
+    print(hypothesis)
+    print(referenceArray)
 
     print("Comparing kwsfile and adjustedhyp")
     subProcessStartedTime = time.time()
-    results = compare(kwsfile, adjustedhyp)
+    results = compare(referenceInputFile, hypothesis)
     print("Process took %s seconds" % (time.time() - subProcessStartedTime))
     print(results)
 
@@ -87,6 +83,6 @@ if __name__ == "__main__":
     substitutions = results['Subs']
     deletions = results['Del']
 
-    wordErrorRate = float(insertions + deletions + substitutions) / len(refs) * 100
+    wordErrorRate = float(insertions + deletions + substitutions) / len(referenceArray) * 100
     print("Word error rate:" + str(wordErrorRate))
     print("Entire process completed in %s seconds" % (time.time() - processStartedTime))
