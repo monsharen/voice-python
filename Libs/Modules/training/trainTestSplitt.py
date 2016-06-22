8270482cbdbc1559b3799d8b71d780989a811665
Axel Ericsson <axel.ericsson@gmail.com>
2016-06-22 19:50:14 +0100

    Updated Pipeline

--- Libs/Modules/training/trainTestSplitt.py
+++ Libs/Modules/training/trainTestSplitt.py
@@ -5,7 +5,7 @@ __author__ = 'a.ericsson'
 ### cut original file and pass to test folder, read and merge all subs into one file.
 ### adjust fileid file and transcription file
 
-def trainTestSplit(train=0.95,subArray=None):
+def trainTestSplit(train=0.6,subArray=None):
     size =  len(subArray)
     trainIndex = int(train * size)
     trainFiles = subArray[0:trainIndex]
@@ -13,7 +13,7 @@ def trainTestSplit(train=0.95,subArray=None):
     return [trainFiles,testFiles]
 
 def generateTranscript(testFiles, rawTranscription):
-    testTranscription=" ".join([element[2] for element in testFiles] )
+    testTranscription=" ".join([element[-1] for element in testFiles] )
     transcription = open(rawTranscription,'w',encoding="utf8")
     transcription.write(testTranscription + " ")
     transcription.close()
